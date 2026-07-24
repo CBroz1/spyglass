5446aae9442995241afe2510c134e8fba4b742f5
CBroz1 <CBrozdowski@yahoo.com>
2026-07-24 11:08:13 -0700

    PR1: remove poor-specificity tests

--- tests/common/test_interval.py
+++ tests/common/test_interval.py
@@ -1,10 +1,10 @@
 # pylint: disable=protected-access
 
+from unittest.mock import patch
+
 import numpy as np
-import pandas as pd
 import pytest
 from numpy import array_equal
-from unittest.mock import patch
 
 
 @pytest.fixture(scope="session", name="interval_list")
@@ -557,36 +557,3 @@ def test_deprecated_functions():
         # Test interval_list_union
         result = interval_list_union([[1.0, 2.0]], [[3.0, 4.0]])
         mock_log.assert_called()
-
-
-def test_interval_list_operations_from_targeted():
-    """Basic instantiation path for IntervalList."""
-    from spyglass.common.common_interval import IntervalList
-
-    interval_list = IntervalList()
-    assert interval_list is not None
-
-
-@pytest.mark.parametrize(
-    "test_input,expected_type",
-    [
-        ([1, 2, 3], list),
-        (np.array([1, 2, 3]), np.ndarray),
-        (pd.Series([1, 2, 3]), pd.Series),
-    ],
-)
-def test_data_type_handling_from_targeted(test_input, expected_type):
-    """Sanity-check expected data container types."""
-    assert isinstance(test_input, expected_type)
-
-
-def test_data_validation_edge_cases_from_targeted():
-    """Basic DataFrame edge-case sanity checks."""
-    empty_data = pd.DataFrame()
-    assert len(empty_data) == 0
-
-    single_row = pd.DataFrame({"col1": [1], "col2": [2]})
-    assert len(single_row) == 1
-
-    data_with_nan = pd.DataFrame({"col1": [1, np.nan, 3]})
-    assert bool(data_with_nan.isna().any().any())
